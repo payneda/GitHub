2c4c1a2c7d8061998dc90b707448921c0969ef31
Daniel Payne <payneda@my.wccc.edu>
2015-09-01 20:02:15 -0400

    Assignment
    this my 5 go at this. Im am hopeful.

---
+++ DanielPayneAssignment1.5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package danielpayneassignment1;
+
+/**
+ *
+ * @author Home pc
+ */
+public class DanielPayneAssignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int letter;
+        char letter = 0;
+        int x = (int)letter;
+        
+        System.out.println("ascci" + letter);
+        
+    }
+    
+}
